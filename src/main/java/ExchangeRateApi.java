55e0f159eaecb9f455443de8745d16eca31d78c7
Bruno <brunohenrique0665@gmail.com>
2025-04-21 14:06:00 -0300

    update api connection

--- src/main/java/ExchangeRateApi.java
+++ src/main/java/ExchangeRateApi.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class ExchangeRateApi {
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ExchangeRateApi {
+
+    private String apiKey;
+    private String address;
+    private HttpClient client;
+    private HttpRequest request;
+
+    public ExchangeRateApi() {
+        this.apiKey = "c19d9fdf00e44cd9f44a66b1";
+        this.address = "https://v6.exchangerate-api.com/v6/"
+            + this.apiKey + "/latest/USD";
+        this.client = HttpClient.newHttpClient();
+        this.request = HttpRequest.newBuilder()
+            .uri(URI.create(this.address))
+            .build();
+    }
+
+    public String makeReques() {
+        String json = "";
+        try {
+            HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+            json = response.body();
+
+        } catch (IOException | InterruptedException e) {
+            System.out.println("Opps. Algo deu errado, entre em contato com o suporte.");
+        }
+
+        return json;
+    }
 }
